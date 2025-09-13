68c714a46bbc54f1d226612dcc29af08f755656e
DevarajSrinivasan <57531195+DevarajSrinivasan@users.noreply.github.com>
2025-09-13 21:41:42 +0530

    column name issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'mobile_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
