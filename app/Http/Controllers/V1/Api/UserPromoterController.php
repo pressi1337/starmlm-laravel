3e431c210e9226d54917c563a99ca80bc41a9169
DevarajSrinivasan <57531195+DevarajSrinivasan@users.noreply.github.com>
2025-10-15 20:07:20 +0530

    status fix

--- app/Http/Controllers/V1/Api/UserPromoterController.php
+++ app/Http/Controllers/V1/Api/UserPromoterController.php
@@ -281,7 +281,7 @@ class UserPromoterController extends Controller
         $promoter->updated_by = Auth::id();
         $promoter->save();
         $user = User::find($promoter->user_id);
-        $user->promoter_status = ($user->current_promoter_level == null) ? null : 4;
+        $user->promoter_status = ($user->current_promoter_level === null) ? null : 4;
         $user->save();
         return response()->json([
             'success' => true,
